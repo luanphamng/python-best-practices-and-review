776c365ecb27ae64097de1ced4fb508ab11fa5c3
luanpham107 <luan.sys@gmail.com>
2020-03-07 22:47:37 +0700

    Open camera

---
+++ usefull-python-trick/OpenWebCam.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  6 23:56:21 2020
+
+@author: Liam
+"""
+
+import cv2
+
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
+while rval:
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+cv2.destroyWindow("preview")
